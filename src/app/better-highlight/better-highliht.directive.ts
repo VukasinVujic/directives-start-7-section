c20eea1f6df950f9efc4613498b9d55d266a859f
vukasinvujic <vukasinvujic021@gmail.com>
2021-09-25 11:38:17 +0200

    @HostBinding direktiva

--- src/app/better-highlight/better-highliht.directive.ts
+++ src/app/better-highlight/better-highliht.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   OnInit,
   Renderer2,
@@ -10,23 +11,28 @@ import {
   selector: '[appBetterHighliht]',
 })
 export class BetterHighlihtDirective implements OnInit {
+  @HostBinding('style.backgroundColor') nameBackgroundColor: string =
+    'transparent';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {}
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.nameBackgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+    this.nameBackgroundColor = 'transparent';
   }
 }
