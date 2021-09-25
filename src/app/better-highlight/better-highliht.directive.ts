e4b6e73c619641822e9e72bb8660b35b3817f3ff
vukasinvujic <vukasinvujic021@gmail.com>
2021-09-25 11:06:37 +0200

    @HostListener , hover dekorator

--- src/app/better-highlight/better-highliht.directive.ts
+++ src/app/better-highlight/better-highliht.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighliht]',
@@ -6,11 +12,21 @@ import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 export class BetterHighlihtDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'blue'
+    );
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'transparent'
     );
   }
 }
